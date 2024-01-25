e8606506dba81cbecedc37dc5392e84bf1d8397b
phReger <philipp.reger@tngtech.com>
2024-01-25 15:08:57 +0100

    personalize main.js

--- src/main.js
+++ src/main.js
@@ -4,25 +4,26 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Philipp Reger',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'continuous integration',
+			'continuous delivery'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'Jenkins',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'[insert unknown unknowns here]'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'play board games',
+			'watch tv shows',
+			'chill w/ friends'
 		]
 	}
 });
